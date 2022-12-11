04f9d880a929abfe1cccf055dcc81aa87a15041f
Oleksandr Halitsa@ <alex.dragonfox@gmail.com>
2022-12-11 19:05:27 +0200

    Add ElGamal implementation
    Co-authored-by: Alex Parshyn <ParshinAlex@users.noreply.github.com>
    Co-authored-by: LytvynenkoJ <LytvynenkoJ@users.noreply.github.com>

--- lab-3/big-number.cpp
+++ lab-3/big-number.cpp
@@ -1,129 +1,177 @@
-#include "big-number.hpp";
+#include "./big-number.hpp";
 
 
-class BigNumber
+BigNumber::BigNumber(): value(initEmpty(), ::BN_free) {}
+BigNumber::BigNumber(const int64_t number): value(initDecimal(number), ::BN_free) {}
+BigNumber::BigNumber(const std::string& str): value(initHex(str), ::BN_free) {}
+BigNumber::BigNumber(const BigNumber& obj): value(copy(obj.value), ::BN_free) {}
+
+BigNumber& BigNumber::operator=(const BigNumber& obj)
 {
-    using bigNumberPointer = std::unique_ptr<BIGNUM, decltype(&::BN_free)>;
-    using bigNumberContext = std::unique_ptr<BN_CTX, decltype(&::BN_CTX_free)>;
-    using bigNumberMontgomeryContext = std::unique_ptr<BN_MONT_CTX, decltype(&::BN_MONT_CTX_free)>;
+    if (this != &obj) value.reset(BN_dup(obj.value.get()));
 
-private:
-    bigNumberPointer value;
+    return *this;
+}
 
-public:
-    BigNumber(): value(init(), ::BN_free) {}
-    BigNumber(const std::string &str): value(convertStringToBigNumber(str), ::BN_free) {}
-    BigNumber(const BigNumber& obj): value(copy(obj.value), ::BN_free) {}
+BigNumber BigNumber::add(const BigNumber& firstOperand, const BigNumber& secondOperand)
+{
+    BigNumber result;
 
-    BigNumber& operator=(const BigNumber& obj)
-    {
-        if (this != &obj)
-            value.reset(BN_dup(obj.value.get()));
+    BN_add(result.value.get(), firstOperand.value.get(), secondOperand.value.get());
 
-        return *this;
-    }
+    return result;
+}
 
-    static BigNumber add(const BigNumber& firstOperand, const BigNumber& secondOperand)
-    {
-        BigNumber result;
+BigNumber BigNumber::sub(const BigNumber& firstOperand, const BigNumber& secondOperand)
+{
+    BigNumber result;
 
-        BN_add(result.value.get(), firstOperand.value.get(), secondOperand.value.get());
+    BN_sub(result.value.get(), firstOperand.value.get(), secondOperand.value.get());
 
-        return result;
-    }
+    return result;
+}
 
-    static BigNumber sub(const BigNumber& firstOperand, const BigNumber& secondOperand)
-    {
-        BigNumber result;
+BigNumber BigNumber::mod(const BigNumber& number, const BigNumber& modulo)
+{
+    BigNumber result;
+    bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
 
-        BN_sub(result.value.get(), firstOperand.value.get(), secondOperand.value.get());
+    BN_nnmod(result.value.get(), number.value.get(), modulo.value.get(), ctx.get());
 
-        return result;
-    }
+    return result;
+}
 
-    static BigNumber mod(const BigNumber& number, const BigNumber& modulo)
-    {
-        BigNumber result;
-        bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
+BigNumber BigNumber::add_mod(const BigNumber& firstOperand, const BigNumber& secondOperand, const BigNumber& modulo)
+{
+    BigNumber result;
+    bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
 
-        BN_mod(result.value.get(), number.value.get(), modulo.value.get(), ctx.get());
+    BN_mod_add(result.value.get(), firstOperand.value.get(), secondOperand.value.get(), modulo.value.get(), ctx.get());
 
-        return result;
-    }
+    return result;
+}
 
-    static BigNumber add_mod(const BigNumber& firstOperand, const BigNumber& secondOperand, const BigNumber& modulo)
-    {
-        BigNumber result;
-        bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
+BigNumber BigNumber::mul(const BigNumber& firstOperand, const BigNumber& secondOperand)
+{
+    BigNumber result;
+    bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
 
-        BN_mod_add(result.value.get(), firstOperand.value.get(), secondOperand.value.get(), modulo.value.get(), ctx.get());
+    BN_mul(result.value.get(), firstOperand.value.get(), secondOperand.value.get(), ctx.get());
 
-        return result;
-    }
+    return result;
+}
 
-    static BigNumber mul(const BigNumber& firstOperand, const BigNumber& secondOperand)
-    {
-        BigNumber result;
-        bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
-        
-        BN_mul(result.value.get(), firstOperand.value.get(), secondOperand.value.get(), ctx.get());
+BigNumber BigNumber::mul_mod(const BigNumber& firstOperand, const BigNumber& secondOperand, const BigNumber& modulo)
+{
+    BigNumber result;
+    bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
 
-        return result;
-    }
+    BN_mod_mul(result.value.get(), firstOperand.value.get(), secondOperand.value.get(), modulo.value.get(), ctx.get());
 
-    static BigNumber mul_mod(const BigNumber& firstOperand, const BigNumber& secondOperand, const BigNumber& modulo)
-    {
-        BigNumber result;
-        bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
-        bigNumberMontgomeryContext montCtx(BN_MONT_CTX_new(), ::BN_MONT_CTX_free);
+    return result;
+}
 
-        BN_mod_mul_montgomery(result.value.get(), firstOperand.value.get(), secondOperand.value.get(), montCtx.get(), ctx.get());
+BigNumber BigNumber::div(const BigNumber& firstOperand, const BigNumber& secondOperand)
+{
+    BigNumber result;
+    bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
 
-        return result;
-    }
+    BN_div(result.value.get(), nullptr, firstOperand.value.get(), secondOperand.value.get(), ctx.get());
 
-    static BigNumber exp_mod(const BigNumber& number, const BigNumber& power, const BigNumber& modulo)
-    {
-        BigNumber result;
-        bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
-        bigNumberMontgomeryContext montCtx(BN_MONT_CTX_new(), ::BN_MONT_CTX_free);
+    return result;
+}
 
-        BN_mod_exp_mont(result.value.get(), number.value.get(), power.value.get(), modulo.value.get(), ctx.get(), montCtx.get());
+BigNumber BigNumber::sqr_mod(const BigNumber& number, const BigNumber& modulo)
+{
+    return BigNumber::mul_mod(number, number, modulo);
+}
 
-        return result;
-    }
+BigNumber BigNumber::exp_mod(const BigNumber& number, const BigNumber& power, const BigNumber& modulo)
+{
+    BigNumber result;
+    bigNumberContext ctx(BN_CTX_new(), ::BN_CTX_free);
 
-    friend std::ostream& operator<<(std::ostream& out, const BigNumber& number);
+    BN_mod_exp(result.value.get(), number.value.get(), power.value.get(), modulo.value.get(), ctx.get());
 
-protected:
-    static BIGNUM* init()
-    {
-        BIGNUM* z = BN_new();
-        BN_zero(z);
-        
-        return z;
-    }
+    return result;
+}
 
-    static BIGNUM* convertStringToBigNumber(const std::string &hexString)
-    {
-        BIGNUM* t = BN_new();
-        BN_hex2bn(&t, hexString.c_str());
-        
-        return t;
-    }
+BIGNUM* BigNumber::initEmpty()
+{
+    auto bigNumber = BN_new();
+    BN_zero(bigNumber);
 
-    static BIGNUM* copy(const bigNumberPointer& arg)
-    {
-        return BN_dup(arg.get());
-    }
-};
+    return bigNumber;
+}
+
+BIGNUM* BigNumber::initDecimal(const int64_t& number)
+{
+    auto bigNumber = BN_new();
+    BN_dec2bn(&bigNumber, std::to_string(number).c_str());
+
+    return bigNumber;
+}
+
+BIGNUM* BigNumber::initHex(const std::string& hexString)
+{
+    auto bigNumber = BN_new();
+    BN_hex2bn(&bigNumber, hexString.c_str());
+
+    return bigNumber;
+}
+
+BIGNUM* BigNumber::copy(const bigNumberPointer& arg)
+{
+    return BN_dup(arg.get());
+}
+
+BigNumber rand_big_number(const uint64_t& bitsCount, const int8_t& firstSignificantBit)
+{
+    BigNumber randomNumber;
+    BN_rand(randomNumber.value.get(), bitsCount - 1, firstSignificantBit, false);
+
+    return randomNumber;
+}
+
+BigNumber rand_prime_big_number(const uint64_t& bitsCount)
+{
+    BigNumber randomPrime;
+    BN_generate_prime_ex(randomPrime.value.get(), bitsCount, 1, NULL, NULL, NULL);
+
+    return randomPrime;
+}
 
 BigNumber operator+(const BigNumber& firstOperand, const BigNumber& secondOperand)
 {
     return BigNumber::add(firstOperand, secondOperand);
 }
 
+BigNumber operator+(const int64_t& firstOperand, const BigNumber& secondOperand)
+{
+    return BigNumber::add(firstOperand, secondOperand);
+}
+
+BigNumber operator+(const BigNumber& firstOperand, const int64_t& secondOperand)
+{
+    return BigNumber::add(firstOperand, secondOperand);
+}
+
+BigNumber operator-(const BigNumber& number)
+{
+    return -1 * number;
+}
+
 BigNumber operator-(const BigNumber& firstOperand, const BigNumber& secondOperand)
+{
+    return BigNumber::sub(firstOperand, secondOperand);
+}
+
+BigNumber operator-(const int64_t& firstOperand, const BigNumber& secondOperand)
+{
+    return BigNumber::sub(firstOperand, secondOperand);
+}
+
+BigNumber operator-(const BigNumber& firstOperand, const int64_t& secondOperand)
 {
     return BigNumber::sub(firstOperand, secondOperand);
 }
@@ -133,9 +181,59 @@ BigNumber operator*(const BigNumber& firstOperand, const BigNumber& secondOperan
     return BigNumber::mul(firstOperand, secondOperand);
 }
 
+BigNumber operator*(const int64_t& firstOperand, const BigNumber& secondOperand)
+{
+    return BigNumber::mul(firstOperand, secondOperand);
+}
+
+BigNumber operator*(const BigNumber& firstOperand, const int64_t& secondOperand)
+{
+    return BigNumber::mul(firstOperand, secondOperand);
+}
+
+BigNumber operator/(const BigNumber& firstOperand, const BigNumber& secondOperand)
+{
+    return BigNumber::div(firstOperand, secondOperand);
+}
+
+BigNumber operator/(const int64_t& firstOperand, const BigNumber& secondOperand)
+{
+    return BigNumber::div(firstOperand, secondOperand);
+}
+
+BigNumber operator/(const BigNumber& firstOperand, const int64_t& secondOperand)
+{
+    return BigNumber::div(firstOperand, secondOperand);
+}
+
 BigNumber operator%(const BigNumber& firstOperand, const BigNumber& secondOperand)
 {
     return BigNumber::mod(firstOperand, secondOperand);
+}
+
+BigNumber operator%(const int64_t& firstOperand, const BigNumber& secondOperand)
+{
+    return BigNumber::mod(firstOperand, secondOperand);
+}
+
+BigNumber operator%(const BigNumber& firstOperand, const int64_t& secondOperand)
+{
+    return BigNumber::mod(firstOperand, secondOperand);
+}
+
+bool operator==(const BigNumber& firstOperand, const BigNumber& secondOperand)
+{
+    return BN_cmp(firstOperand.value.get(), secondOperand.value.get()) == 0;
+}
+
+bool operator<(const BigNumber& firstOperand, const BigNumber& secondOperand)
+{
+    return BN_cmp(firstOperand.value.get(), secondOperand.value.get()) == -1;
+}
+
+bool operator>(const BigNumber& firstOperand, const BigNumber& secondOperand)
+{
+    return BN_cmp(firstOperand.value.get(), secondOperand.value.get()) == 1;
 }
 
 std::ostream& operator<<(std::ostream& out, const BigNumber& number)
@@ -143,19 +241,8 @@ std::ostream& operator<<(std::ostream& out, const BigNumber& number)
     const long f = out.flags() & std::ios::basefield;
     char* ptr = nullptr;
 
-    if (f == std::ios::hex)
-    {
-        ptr = BN_bn2hex(number.value.get());
-        out << ptr;
-    }
-    else if (f == std::ios::dec)
-    {
-        ptr = BN_bn2dec(number.value.get());
-        out << ptr;
-    }
-    else throw std::runtime_error("Not implemented");
-
-    if (ptr) OPENSSL_free(ptr);
+    ptr = BN_bn2hex(number.value.get());
+    out << ptr;
 
     return out;
 }
